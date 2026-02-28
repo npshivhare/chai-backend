8b1c243db9029e66c635517671c17248aa13cd6d
Nrependre Shivhare <npshivhare392003@gmail.com>
2026-02-28 14:16:01 +0530

    All setup configured

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-        console.log('\n MongoDB connected successfully!! DB HOST: ${connectionInstance}');
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("Error connecting to MongoDB:", error);
         process.exit(1);
